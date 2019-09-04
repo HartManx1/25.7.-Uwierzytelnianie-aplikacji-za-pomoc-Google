aeb1268de4b71898b619cd16bc2e95756cbf6759
Martin Hartman <hartmanx1@wp.pl>
2019-09-04 22:00:29 +0200

    25.7 done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '950362368404-9p7la2tb0ue3f1fim9pqmlqpacfd03tp.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'h4wIbqcWHeuOMJRtR6O1p-j-',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
